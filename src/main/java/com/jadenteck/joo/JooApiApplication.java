d6278aa32871fbdd0c09b0f8a1cedb57b196e352
joo-pe <joohyung1002@naver.com>
2020-12-21 07:10:38 +0900

    git revert

---
+++ src/main/java/com/jadenteck/joo/JooApiApplication.java
@@ -0,0 +1,11 @@
+package com.jadenteck.joo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JooApiApplication.class, args);
+    }
+}
